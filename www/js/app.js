ee9a301fc7d878cd99feb84a650085e365c818e4
matt1123 <matthewkrause@dhcp152-54-6-191.wireless.europa.renci.org>
2015-07-06 14:16:52 -0400

    Angular Route Provider Fix

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,16 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  //'myApp.home',
   'myApp.login',
     'ngRoute',
     'httpInterceptorModule',
     'MessageCenter',
-    'globalsModule'
+    'globalsModule',
 
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/login'}); 
+  $routeProvider.otherwise({redirectTo: '/home'}); //was '/login'
 }]).config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.withCredentials = true;
 }]);
