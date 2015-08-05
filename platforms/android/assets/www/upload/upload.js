b3f1f0d8d7c1ffeac8d0001e7f7f545d4aec2ef9
matt1123 <matthewkrause@Matthew-MacBook-Pro.local>
2015-08-05 15:12:24 -0400

    iOS Cordova compatible

--- platforms/android/assets/www/upload/upload.js
+++ platforms/android/assets/www/upload/upload.js
@@ -19,6 +19,21 @@ angular.module('upload', ['ngRoute','ngFileUpload'])
         });
 
         $scope.multiple = true;
+
+        $scope.upload = function(files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    Upload.upload({
+                        url: $globals.backendUrl('file'),
+                        fields: {collectionParentName: '/Starred%20Files?offset=0&path='},
+                        file: file
+                    })
+                }
+            }
+        }
+
+        /*
         $scope.upload = function (files) {
             if (files && files.length) {
                 $(".upload_container").css('display','none');
@@ -27,10 +42,10 @@ angular.module('upload', ['ngRoute','ngFileUpload'])
                 for (var i = 0; i < files.length; i++) {
                     var file = files[i];
 
-                    $(".upload_container_result ul").append('<li id="uploading_item_'+i+'" class="light_back_option_even"><div class="col-xs-7 list_content"><img src="images/data_object_icon.png">'+file.name+'</div></li>');
+                    //$(".upload_container_result ul").append('<li id="uploading_item_'+i+'" class="light_back_option_even"><div class="col-xs-7 list_content"><img src="images/data_object_icon.png">'+file.name+'</div></li>');
 
                     Upload.upload({
-                        url: $globals.backendUrl('file') , /* collection/Starred%20Files?offset=0&path= */
+                        url: $globals.backendUrl('file') , /* collection/Starred%20Files?offset=0&path= *//*
                         fields:{collectionParentName: '/Starred%20Files?offset=0&path='},
                         file: file
                     }).progress(function (evt) {
@@ -42,6 +57,7 @@ angular.module('upload', ['ngRoute','ngFileUpload'])
                 }
             }
         };
+*/
 
     }])
 
