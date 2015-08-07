6e3371a838ddb2fe0cccdbbe0a27854625a84470
matt1123 <matthewkrause@dhcp152-54-6-191.wireless.europa.renci.org>
2015-08-07 13:32:27 -0400

    Fix Search Bug

---
+++ platforms/ios/www/upload/upload.js
@@ -0,0 +1,67 @@
+'use strict';
+
+angular.module('upload', ['ngRoute','ngFileUpload'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/upload', {
+            templateUrl: 'upload/upload.html',
+            controller: 'uploadCtrl',
+
+        });
+    }])
+
+    //--------Camera--------
+    //
+    //.directive('camera', function() {
+    //    return {
+    //        restrict: 'A',
+    //        require: 'ngModel',
+    //        link: function(scope, elm, attrs, ctrl) {
+    //            elm.on('click', function() {
+    //                navigator.camera.getPicture(function (imageURI) {
+    //                    scope.$apply(function() {
+    //                        ctrl.$setViewValue(imageURI);
+    //                    });
+    //                }, function (err) {
+    //                    ctrl.$setValidity('error', false);
+    //                }, { quality: 50, destinationType: Camera.DestinationType.FILE_URI }
+    //                )});
+    //        }
+    //    };
+    //})
+
+    .controller('uploadCtrl', ['$scope', 'Upload', 'globals', function ($scope, Upload, $globals) {
+
+
+        //--------Camera--------
+
+        //$scope.myPictures = [];
+        //$scope.$watch('myPicture', function(value) {
+        //    if(value) {
+        //        myPictures.push(value);
+        //    }
+        //}, true);
+
+        //--------Camera END-----
+
+        $scope.$watch('files', function () {
+            $scope.upload($scope.files);
+        });
+
+        $scope.multiple = true;
+
+        $scope.upload = function(files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    Upload.upload({
+                        url: $globals.backendUrl('file'),
+                        fields: {collectionParentName: '/Starred%20Files?offset=0&path='},
+                        file: file
+                    })
+                }
+            }
+        }
+
+
+    }])
